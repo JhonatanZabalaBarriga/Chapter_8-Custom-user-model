37c41aa2ef156ca55e9960e6fec2bbff16074a2b
JhonatanZabalaBarriga <zabalaba12@gmail.com>
2022-06-23 21:31:38 -0500

    create articles app

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     #local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
 
     #3rd Party
     'crispy_forms',
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
